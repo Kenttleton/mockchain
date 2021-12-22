7fd564f64f681a105032d3d506fc13e5c27811dc
Kent <utterback.kent.a@gmail.com>
2021-12-22 14:32:58 -0800

    split library and better naming

--- pow/node/src/main.rs
+++ pow/node/src/main.rs
@@ -1,14 +1,14 @@
-use cert_gen;
+use cryptography::RSA;
 use env_logger;
-use std::net::TcpListener;
+use std::net::UdpSocket;
 use std::thread::spawn;
 
 fn main() {
     //std::env::set_var("RUST_LOG", "actix_server=info,actix_web=info");
     env_logger::init();
-    cert_gen::create();
-    let server = TcpListener::bind("0.0.0.0:8546").unwrap();
-    for stream in server.incoming() {
-        spawn(move || stream);
-    }
+    let cert = RSA::new(4096);
+    let socket = UdpSocket::bind("0.0.0.0:8546");
 }
+
+// TODO: Update IP list
+// TODO:
