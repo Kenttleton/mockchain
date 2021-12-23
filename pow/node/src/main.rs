f222ee875c651acd4dfd6b139e67f9c4c6c4aaa8
Kent <utterback.kent.a@gmail.com>
2021-12-22 16:43:26 -0800

    fix lib and start udp setup

--- pow/node/src/main.rs
+++ pow/node/src/main.rs
@@ -1,14 +1,22 @@
 use cryptography::RSA;
-use env_logger;
+use ctrlc;
+use std::io::Result;
 use std::net::UdpSocket;
 use std::thread::spawn;
 
 fn main() {
-    //std::env::set_var("RUST_LOG", "actix_server=info,actix_web=info");
-    env_logger::init();
+    // std::env::set_var("RUST_LOG", "actix_server=info,actix_web=info");
+    // env_logger::init();
     let cert = RSA::new(4096);
-    let socket = UdpSocket::bind("0.0.0.0:8546");
+    let socket = UdpSocket::bind("127.0.0.1:8546").unwrap();
+    socket.set_nonblocking(true).unwrap();
+    // 1 MB buffer per Bitcoin spec for a block size
+    let mut buf = [0; 1000000];
+    let (amt, src) = socket.recv_from(&mut buf).unwrap();
+    let buf = &mut buf[..amt];
+    buf.reverse();
+    println!("Source: {}\nAmount: {}", &src, &amt);
+    socket.send_to(buf, &src).unwrap();
 }
 
-// TODO: Update IP list
-// TODO:
+fn handler() {}
